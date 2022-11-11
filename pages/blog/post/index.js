520261abaf3f3dec49c2ed4feabf6342aedf6a6d
Vít Smolík <vit.smolik2@gmail.com>
2022-11-11 21:12:36 +0000

    Přidání dalšího blogpostu

--- pages/blog/post/index.js
+++ pages/blog/post/index.js
@@ -15,7 +15,9 @@ export default function Home() {
     </Head>
     <div className={styles.title}>
         <h1>Všechny blog posty</h1>
-      </div><div className={styles.blogpost}>
+      </div><div>
+      <Link legacyBehavior href='../blog/post/2'><a className={styles.blogtitle}>Next.JS Deployment</a></Link>
+         <p className={styles.blogpost}>Ahoj, tady zas a znova Vítek, dneska se tady chci rozepsat o tom jak jsem deploynul NExt.JS aplikaci (tento web) za použití Githubu a Vercelu...</p>
         <Link legacyBehavior href='../1'><a className={styles.blogtitle}>Jen začátek...</a></Link>
          <p className={styles.blogpost}>Ahoj, tady Vítek a první blog post, jsem rád, že jste se rozhodli přidat se se mnou na tuhle cestu. Tahle cesta bude o tom všem co se snažím naučit, co umím, co dělám...</p>
         </div>
