3fa3b53521176fc79c352a3c441dda7e8fe73d56
Vít Smolík <vit.smolik2@gmail.com>
2022-11-11 10:09:17 +0100

    Snaha o to aby [id].js fungovalo - nepovedlo se

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -16,8 +16,7 @@ export default function Home() {
     <div className={styles.title}>
         <h1>Blog</h1>
       </div><div className={styles.blogpost}>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae semper quis lectus nulla at volutpat diam ut venenatis. Facilisis leo vel fringilla est ullamcorper eget nulla facilisi. Pretium viverra suspendisse potenti nullam ac tortor vitae purus faucibus</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae semper quis lectus nulla at volutpat diam ut venenatis. Facilisis leo vel fringilla est ullamcorper eget nulla facilisi. Pretium viverra suspendisse potenti nullam ac tortor vitae purus faucibus</p>
+         
         </div>
     </>
   )
